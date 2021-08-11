c794c1a62dd5667efb37446d3c8741a44e2081d6
Lee Howard <leegibsonhoward@gmail.com>
2021-08-11 12:20:36 -0700

    chore(updates): update packages and fix tests
    - update all depends
    
    - reimplement jest preprocessor
    
    - docblock to use jsdom in component test files

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,4 +1,8 @@
+const babelJestMd = require('babel-jest');
+const babelJest = babelJestMd.__esModule ? babelJestMd.default : babelJestMd;
+
 const babelOptions = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
-module.exports = require('babel-jest').createTransformer(babelOptions);
+
+module.exports = babelJest.createTransformer(babelOptions);
