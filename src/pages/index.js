ed4e4b74c61087d5ff7825621df7ffe364738964
leegibsonhoward <leegibsonhoward@gmail.com>
2021-12-18 15:58:27 -0800

    feat(pages): move about hero to about pages
    - create scss module for about

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import styles from './index.module.scss';
 import RecentPost from '../components/RecentPost';
@@ -8,34 +7,6 @@ import FeaturedProject, { projects } from '../components/FeaturedProject';
 function Home() {
   return (
     <Layout description="Portfolio, Blog, and Docs all in one place!">
-      <div className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <div className={styles.introContainer}>
-                <h1 className="hero__title">Hi I&#39;m Lee.</h1>
-                <p className={clsx('hero__subtitle', styles.heroSubtitle)}>
-                  I&#39;m a software engineer and enthusiast of most things
-                  tech. This is my personal website which functions as a blog
-                  along with docs of things I&#39;ve learned over the years.
-                  Have a look around you might find something useful!
-                </p>
-              </div>
-            </div>
-            <div className={`col`}>
-              <div className={styles.selfieContainer}>
-                <img
-                  className={styles.selfieImage}
-                  src="img/me.jpg"
-                  height="320"
-                  width="320"
-                ></img>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <main>
         <section className={styles.recentPosts}>
           <div className="container">
