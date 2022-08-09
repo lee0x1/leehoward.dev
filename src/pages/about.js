2d1a687ebf5c618de67cf0c04afe8a29d8c76401
leehowardx <git@leehoward.dev>
2022-08-08 17:47:18 -0700

    feat(pages): remove about page contents

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,33 +7,6 @@ import selfieImage from '@site/static/img/me.jpg';
 function About() {
   return (
     <Layout description="Portfolio, Blog, and Docs all in one place!">
-      <div className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <div className={styles.introContainer}>
-                <h1 className="hero__title">Hi I&#39;m Lee.</h1>
-                <p className={clsx('hero__subtitle', styles.heroSubtitle)}>
-                  I&#39;m a software engineer and enthusiast of most things
-                  tech. This is my personal website which functions as a blog
-                  along with docs of things I&#39;ve learned over the years.
-                  Have a look around you might find something useful!
-                </p>
-              </div>
-            </div>
-            <div className={`col`}>
-              <div className={styles.selfieContainer}>
-                <img
-                  className={styles.selfieImage}
-                  src={selfieImage}
-                  height="320"
-                  width="320"
-                ></img>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <main></main>
     </Layout>
   );
