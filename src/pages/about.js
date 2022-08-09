96eca7cf9ab486cb9247d5627f0e8d8c07400665
leehowardx <git@leehoward.dev>
2022-08-08 17:55:08 -0700

    fix(pages): remove remaining about page code

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import styles from './about.module.scss';
-import clsx from 'clsx';
-import selfieImage from '@site/static/img/me.jpg';
 
 function About() {
   return (
