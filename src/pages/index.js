93fbb640cfc050af653e91be20f2e40ef5712c1a
leehowardx <git@leehoward.dev>
2022-08-14 23:24:17 -0700

    ui(pages): add intro to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import styles from './index.module.scss';
+// import styles from './index.module.scss';
 // import FeaturedProject, { projects } from '../components/FeaturedProject';
 
 function Home() {
   return (
     <Layout description="Portfolio, Blog, and Docs all in one place!">
       <main>
-        <section className={styles.recentPosts}>
+        <div className="hero">
+          <div className="container">
+            <span className="hero__title">Hi, Im Lee.</span>
+            <p className="hero__subtitle">
+              Game Dev at{' '}
+              <a href="https://twitter.com/pixelpackdgames">PixelPackd Games</a>
+            </p>
+            <p>
+              Of many things I enjoying creating pixel art, learning game
+              composition, and contributing to open-source
+            </p>
+          </div>
+        </div>
+        {/* <section className={styles.recentPosts}>
           <div className="container">
             <div className="row"></div>
           </div>
-        </section>
+        </section> */}
 
         {/* {projects && projects.length > 0 && (
           <>
