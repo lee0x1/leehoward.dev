b2638d4987bdf3fc48fed9f052128c07eeb93beb
leehowardx <git@leehoward.dev>
2022-08-16 13:43:30 -0700

    feat(pages): fancy up homepage
    - handle layout styling with simple wrapper class
    
    - more semantic html

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,17 @@ import Layout from '@theme/Layout';
 function Home() {
   return (
     <Layout title="Home" description="Introduction to Lee Howard's website">
-      <div className="hero">
-        <div className="container">
-          <span className="hero__title">Hi, I&apos;m Lee. 👋🏽</span>
-          <p className="hero__subtitle">
-            Game Developer at{' '}
-            <a href="https://twitter.com/pixelpackdgames">PixelPackd Games</a>
-          </p>
-          <p>
-            I like to write about game development and of many things contribute
-            to open-source.
-          </p>
+      <div className="home-wrapper">
+        <div className="hero">
+          <div className="container">
+            <h1 className="hero__title">Hi, I&apos;m Lee. 👋🏽</h1>
+            <p className="hero__subtitle">
+              Game Developer at{' '}
+              <a href="https://twitter.com/pixelpackdgames">PixelPackd Games</a>
+              <br />I like to write about game development and of many things
+              contribute to open-source.
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
